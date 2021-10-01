f15296057537cfa13f693a1b2cd420245ae58c14
Emmanuel Uchenna <eunithelp@gmail.com>
2021-10-01 05:40:31 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * Code Injection
+ *
+ */
+
+
+// Uncomment below and place in header
+
+// <script type="text/javascript"src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"</script>
+
+
+/// Uncomment below and place in Footer
+
+
+{/* <script type="text/javascript">!function(t,e){t.artibotApi={l:[],t:[],on:function(){this.l.push(arguments)},trigger:function(){this.t.push(arguments)}};var a=!1,i=e.createElement("script");i.async=!0,i.type="text/javascript",i.src="https://app.artibot.ai/loader.js",e.getElementsByTagName("head").item(0).appendChild(i),i.onreadystatechange=i.onload=function(){if(!(a||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState)){new window.ArtiBot({i:"b3d644c8-4c4d-417a-aa0f-1deac305c18e"});a=!0}}}(window,document);</script> */}
+
+{/* <script>
+  $(".dropdown h3").click(function () {
+    $(this).nextUntil("h3").slideToggle("slow");
+  });
+</script> */}
