71cb7a3dfa7252e541428189df6a0f0c43dffd1b
Emmanuel Uchenna <eunithelp@gmail.com>
2021-11-24 21:26:29 +0100

    Add: Add script for toggling nav

--- js/script.js
+++ js/script.js
@@ -1,23 +1,48 @@
-
 /**
  *
- * Code Injection
+ * Your custom code Injection below
  *
  */
 
 
-// Uncomment below and place in header
+// ArtibotApi
 
-// <script type="text/javascript"src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"</script>
+! function (t, e) {
+	t.artibotApi = {
+		l: [],
+		t: [],
+		on: function () {
+			this.l.push(arguments);
+		},
+		trigger: function () {
+			this.t.push(arguments);
+		}
+	};
+	var a = !1,
+		i = e.createElement("script");
+	i.async = !0, i.type = "text/javascript", i.src = "https://app.artibot.ai/loader.js", e.getElementsByTagName("head")
+		.item(0).appendChild(i), i.onreadystatechange = i.onload = function () {
+			if (!(a || this.readyState && "loaded" != this.readyState && "complete" != this.readyState)) {
+				new window.ArtiBot({
+					i: "b3d644c8-4c4d-417a-aa0f-1deac305c18e"
+				});
+				a = !0;
+			}
+		};
+}(window, document);
 
+// FAQs
+$(".dropdown h3").click(function () {
+	$(this).nextUntil("h3").slideToggle("slow");
+});
 
-/// Uncomment below and place in Footer
+// Navigation
 
-
-{/* <script type="text/javascript">!function(t,e){t.artibotApi={l:[],t:[],on:function(){this.l.push(arguments)},trigger:function(){this.t.push(arguments)}};var a=!1,i=e.createElement("script");i.async=!0,i.type="text/javascript",i.src="https://app.artibot.ai/loader.js",e.getElementsByTagName("head").item(0).appendChild(i),i.onreadystatechange=i.onload=function(){if(!(a||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState)){new window.ArtiBot({i:"b3d644c8-4c4d-417a-aa0f-1deac305c18e"});a=!0}}}(window,document);</script> */}
-
-{/* <script>
-  $(".dropdown h3").click(function () {
-    $(this).nextUntil("h3").slideToggle("slow");
-  });
-</script> */}
+function toggleNav() {
+	var x = document.getElementById("nav");
+	if (x.className === "nav") {
+		x.className += " responsive";
+	} else {
+		x.className = "nav";
+	}
+}
