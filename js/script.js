068bd439ee42533013e31694074934d8369eddad
Ξunit <eunithelp@gmail.com>
2022-08-11 15:38:02 +0100

    Refactor

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ let stateCheck = setInterval(() => {
 
     let popupText = document.getElementById("popupText"),
       menuText = "All rates are subject to change at anytime!",
-      overBookedText = "We are booked until October 29<sup>tt</sup>. Please fill out a <a href='/order-now'>catering form</a> for other enquiries.",
+      overBookedText = "We are booked until October 29<sup>th</sup>. Please fill out a <a href='/order-now'>catering form</a> for other enquiries.",
       cartText = "All rates are subject to change at anytime!",
       checkoutText = "Your order will be ready on or before 4 days time.",
       orderNowText = "Click <a target='_blank' href='https://calendly.com/mknyc/15min'>here</a> to view the earliest possible day for delivery.";
